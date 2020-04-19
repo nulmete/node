0973d2711f85e42259d8105d2b2d5c662c478b47
nulmete <nicoulmete1@gmail.com>
2020-04-18 22:25:51 -0300

    connect to MongoDB

--- express/app.js
+++ express/app.js
@@ -2,16 +2,10 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
+// const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database');
 
 const app = express();
 
@@ -24,80 +18,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // note: in those html files, we have to assume that we're already in the public dir
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    User
-        .findByPk(1)
-        .then(user => { // user = Sequelize object
-            // add user field to req object (IT WILL BE AVAILABLE IN EVERY REQUEST)
-            req.user = user;
-            next();
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
+// app.use((req, res, next) => {
+//     User
+//         .findByPk(1)
+//         .then(user => { // user = Sequelize object
+//             // add user field to req object (IT WILL BE AVAILABLE IN EVERY REQUEST)
+//             req.user = user;
+//             next();
+//         })
+//         .catch(err => {
+//             console.log(err);
+//         });
+// });
 
 // prefix '/admin' to adminRoutes
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
-app.use(errorController.get404);
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
+// app.use(errorController.get404);
 
-// PRODUCT-USER RELATIONS: adds userId to products table
-Product.belongsTo(User, { // This refers to a product owned by an admin user
-    constraints: true,
-    onDelete: 'CASCADE' // if user is deleted -> product is deleted
+mongoConnect(client => {
+    console.log(client);
+    app.listen(3000);
 });
-User.hasMany(Product); // OPTIONAL
-
-// CART-USER RELATIONS: adds userId to cart table
-Cart.belongsTo(User);
-User.hasOne(Cart);
-
-// CART-PRODUCT RELATIONS
-// Since it is Many-to-Many, we need a new table that holds productId and cartId
-// So, we connect them through CartItem
-Cart.belongsToMany(Product, { through: CartItem }); // one Cart can hold many Products
-Product.belongsToMany(Cart, { through: CartItem }); // a Product can be in different Carts
-
-// ORDER-USER RELATIONS: adds userId to orders table
-Order.belongsTo(User);
-User.hasMany(Order);
-
-// ORDER-PRODUCT RELATIONS
-// Since it is Many-to-Many, we need a new table that holds productId and cartId
-// So, we connect them through OrderItem
-Order.belongsToMany(Product, { through: OrderItem }); // one Order can hold many Products
-Product.belongsToMany(Order, { through: OrderItem }); // a Product can be in different Orders
-
-
-// Create tables for defined models
-sequelize
-    // .sync({
-    //     // just for development, not production (to replace tables and apply the newly defined relations)
-    //     force: true
-    // })
-    .sync()
-    .then(result => {
-        return User.findByPk(1);
-    })
-    .then(user => {
-        if (!user) {
-            return User.create({
-                name: 'Nico',
-                email: 'nicoulmete1@gmail.com'
-            });
-        }
-
-        // make sure that we return a Promise instead of a value
-        return Promise.resolve(user);
-    })
-    .then(user => {
-        return user.createCart();
-        // console.log(user);
-    })
-    .then(cart => {
-        app.listen(3000);
-    })
-    .catch(err => {
-        console.log(err);
-    });
--- express/util/database.js
+++ express/util/database.js
@@ -1,8 +1,16 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'root', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
+const mongoConnect = (callback) => {
+    MongoClient
+        .connect('mongodb+srv://nicolas:nicolas@cluster0-xf55q.mongodb.net/test?retryWrites=true&w=majority')
+        .then(client => {
+            console.log('connected');
+            callback(client);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
