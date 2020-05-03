4e6203a8fc22aee5f05e1df3e010d8d375d6caab
nulmete <nicoulmete1@gmail.com>
2020-05-02 22:48:58 -0300

    file upload/delete and pdf generator

--- express/controllers/admin.js
+++ express/controllers/admin.js
@@ -1,6 +1,8 @@
-const { validationResult } = require('express-validator');
-const Product = require('../models/product');
+const fileHelper = require('../util/file');
 const mongoose = require('mongoose');
+const { validationResult } = require('express-validator');
+
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
@@ -149,7 +151,9 @@ exports.postEditProduct = (req, res, next) => {
             product.price = updatedPrice;
             product.description = updatedDescription;
             
+            // if I chose a new image to upload, update the imageUrl and remove old img
             if (image) {
+                fileHelper.deleteFile(product.imageUrl);
                 product.imageUrl = image.path;
             } 
 
@@ -171,16 +175,24 @@ exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
 
     Product
+    .findById(prodId)
+    .then(product => {
+        if (!product) {
+            return next(new Error('Product not found'));
+        }
+        fileHelper.deleteFile(product.imageUrl);
+
         // prevent users from deleting another user's product
-        .deleteOne({ _id: prodId, userId: req.user._id })
-        .then(() => {
-            res.redirect('/admin/products');
-        })
-        .catch(err => {
-            const error = new Error(err);
-            error.httpStatusCode = 500;
-            return next(error);
-        });
+        return Product.deleteOne({ _id: prodId, userId: req.user._id });
+    })
+    .then(() => {
+        res.redirect('/admin/products');
+    })
+    .catch(err => {
+        const error = new Error(err);
+        error.httpStatusCode = 500;
+        return next(error);
+    });
 };
 
 exports.getProducts = (req, res, next) => {
--- express/controllers/shop.js
+++ express/controllers/shop.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+const PDFDocument = require('pdfkit');
+
 const Product = require('../models/product');
 const Order = require('../models/order');
 
@@ -127,6 +131,7 @@ exports.postOrder = (req, res, next) => {
                 }
             });
 
+            
             const order = new Order({
                 user: {
                     email: req.user.email,
@@ -165,4 +170,64 @@ exports.getOrders = (req, res, next) => {
             error.httpStatusCode = 500;
             return next(error);
         });
+};
+
+exports.getInvoice = (req, res, next) => {
+    const orderId = req.params.orderId;
+
+    Order
+        .findById(orderId)
+        .then(order => {
+            if (!order) {
+                return next(new Error('No order found'));
+            }
+
+            if (order.user.userId.toString() !== req.user._id.toString()) {
+                return next(new Error('Unauthorized'));
+            }
+
+            const invoiceName = 'invoice-' + orderId + '.pdf';
+            const invoicePath = path.join('data', 'invoices', invoiceName);
+
+            // pdfDoc is a readableStream
+            const pdfDoc = new PDFDocument();
+
+            res.setHeader('Content-Type', 'application/pdf');
+            //res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
+            res.setHeader('Content-Disposition', 'attachment; filename="' + invoiceName + '"');
+
+            // open pipes
+            pdfDoc.pipe(fs.createWriteStream(invoicePath));
+            pdfDoc.pipe(res);
+
+            // set PDF text
+            pdfDoc.fontSize(20).text('Invoice', {
+                underline: true
+            });
+
+            pdfDoc.text('---------------------------');
+
+            let totalPrice = 0;
+            order.products.forEach(prod => {
+                totalPrice += prod.quantity * prod.product.price;
+
+                pdfDoc.fontSize(14).text(
+                    prod.product.title +
+                    ' - ' +
+                    prod.quantity +
+                    ' x $' +
+                    prod.product.price
+                );
+            });
+
+            pdfDoc.text('Total price: $' + totalPrice);
+
+            // close pipes and send response
+            pdfDoc.end();
+        })
+        .catch(err => {
+            return next(err);
+        });
+
+    
 };
