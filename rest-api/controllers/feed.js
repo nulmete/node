53deaaaafee682bdd5c7d3abbf574c66de21a1d2
nulmete <nicoulmete1@gmail.com>
2020-05-09 22:25:39 -0300

    create posts on the frontend

--- rest-api/controllers/feed.js
+++ rest-api/controllers/feed.js
@@ -1,22 +1,44 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [{
+            _id: '1',
             title: 'First Post',
-            content: 'This is the first post.'
+            content: 'This is the first post.',
+            imageUrl: 'images/infraestructuraRed.jpg',
+            creator: {
+                name: 'Nicolas'
+            },
+            createdAt: new Date()
         }]
     });
 };
 
 exports.createPost = (req, res, next) => {
+    // extract validation errors from route middleware
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            message: 'Validation failed, entered data is incorrect.',
+            errors: errors.array()
+        });
+    }
+
     const title = req.body.title;
     const content = req.body.content;
 
     res.status(201).json({
         message: 'Post created successfully!',
         post: {
-            id: new Date().getTime(),
+            _id: new Date().getTime(),
             title,
-            content
+            content,
+            creator: {
+                name: 'Nicolas'
+            },
+            createdAt: new Date()
         }
     });
 };
--- rest-api/routes/feed.js
+++ rest-api/routes/feed.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 const feedController = require('../controllers/feed');
 
@@ -8,6 +9,14 @@ const router = express.Router();
 router.get('/posts', feedController.getPosts);
 
 // POST /feed/posts
-router.post('/post', feedController.createPost);
+router.post(
+    '/post',
+    // validation middleware
+    [
+        body('title').trim().isLength({ min: 5 }),
+        body('content').trim().isLength({ min: 5 })
+    ],
+    feedController.createPost
+);
 
 module.exports = router;
