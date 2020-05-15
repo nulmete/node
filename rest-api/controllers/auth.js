90a75ca85d775ffb09413f3484bcad81f1dbdc25
nulmete <nicoulmete1@gmail.com>
2020-05-14 23:15:57 -0300

    async/await

--- rest-api/controllers/auth.js
+++ rest-api/controllers/auth.js
@@ -5,7 +5,7 @@ const config = require('../config');
 
 const User = require('../models/user');
 
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
     // collect validation errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -19,87 +19,77 @@ exports.signup = (req, res, next) => {
     const name = req.body.name;
     const password = req.body.password;
 
-    bcrypt
-        .hash(password, 12)
-        .then(hashedPassword => {
-            // create user
-            const user = new User({
-                email,
-                password: hashedPassword,
-                name
-            });
+    try {
+        const hashedPassword = await bcrypt.hash(password, 12);
 
-            return user.save();
-        })
-        .then(result => {
-            res.status(201).json({
-                message: 'User created!',
-                userId: result._id
-            })
-        })
-        .catch(err => {
-            // server-side error
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            // ASYNC CODE => throwing an error doesn't work here
-            // (it doesn't look for the next error-handling middleware)
-            next(err);
+        // create user
+        const user = new User({
+            email,
+            password: hashedPassword,
+            name
         });
+
+        await user.save()
+
+        res.status(201).json({
+            message: 'User created!'
+        });
+    } catch (err) {
+        // server-side error
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        // ASYNC CODE => throwing an error doesn't work here
+        // (it doesn't look for the next error-handling middleware)
+        next(err);
+    }
 };
 
-exports.login = (req, res, next) => {
+exports.login = async (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
-    let loadedUser;
 
-    // email exists?
-    User
-        .findOne({ email })
-        .then(user => {
-            // user undefined?
-            if (!user) {
-                const error = new Error('User could not be found.');
-                error.statusCode = 401;
-                throw error;
-            }
+    try {
+        const user = await User.findOne({ email });
 
-            // user was found
-            loadedUser = user;
+        // if user is undefined
+        if (!user) {
+            const error = new Error('User could not be found.');
+            error.statusCode = 401;
+            throw error;
+        }
 
-            // compare password with hashed password
-            return bcrypt.compare(password, user.password);
-        })
-        .then(isEqual => {
-            if (!isEqual) {
-                const error = new Error('Wrong password.')
-                error.statusCode = 401;
-                throw error;
-            }
+        // compare password with hashed password
+        const isEqual = bcrypt.compare(password, user.password);
 
-            // password matches, generate JWT
-            const token = jwt.sign(
-                {
-                    email: loadedUser.email,
-                    userId: loadedUser._id.toString()
-                },
-                // secret key
-                config.JWT_SECRET,
-                { expiresIn: '1h' }
-            );
+        if (!isEqual) {
+            const error = new Error('Wrong password.')
+            error.statusCode = 401;
+            throw error;
+        }
 
-            res.status(200).json({
-                token: token,
-                userId: loadedUser._id.toString()
-            });
-        })
-        .catch(err => {
-            // server-side error
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            // ASYNC CODE => throwing an error doesn't work here
-            // (it doesn't look for the next error-handling middleware)
-            next(err);
-        })
+        // password matches, generate JWT
+        const token = jwt.sign(
+            {
+                email: user.email,
+                userId: user._id.toString()
+            },
+            // secret key
+            config.JWT_SECRET,
+            { expiresIn: '1h' }
+        );
+
+        res.status(200).json({
+            token: token,
+            userId: user._id.toString()
+        });
+    } catch (err) {
+        // server-side error
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        // ASYNC CODE => throwing an error doesn't work here
+        // (it doesn't look for the next error-handling middleware)
+        next(err);
+    }   
 };
--- rest-api/controllers/feed.js
+++ rest-api/controllers/feed.js
@@ -7,53 +7,46 @@ const Post = require('../models/post');
 const User = require('../models/user');
 
 
-exports.getPosts = (req, res, next) => {
+exports.getPosts = async (req, res, next) => {
     // get current page from frontend (loadPosts)
     // if it's undefined, always start at page 1
     const currentPage = req.query.page || 1;
     // same value as in the frontend
     // would be better to pass this to the frontend
     const perPage = 2;
-    let totalItems;
+    // let totalItems;
 
-    Post
-        .find()
-        .countDocuments()
-        .then(count => {
-            totalItems = count;
-            return Post
-                .find()
-                // page 1 => skip 0 items
-                // page 2 => skip 2 items
-                // page 3 => skip 4 items
-                .skip((currentPage - 1) * perPage)
-                // limit the amount of items retrieved to 2 items
-                .limit(perPage)
-                // needed to show the author name on the frontend
-                // since it expects post.creator.name
-                .populate('creator');
-        })
-        .then(posts => {
-            res.status(200).json({
-                message: 'Fetched posts successfully.',
-                posts,
-                totalItems
-            });
-        })
-        .catch(err => {
-            // server-side error
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            // ASYNC CODE => throwing an error doesn't work here
-            // (it doesn't look for the next error-handling middleware)
-            next(err);
-        })
+    try {
+        const totalItems = await Post.find().countDocuments();
 
-    
+        const posts = await Post.find()
+            // page 1 => skip 0 items
+            // page 2 => skip 2 items
+            // page 3 => skip 4 items
+            .skip((currentPage - 1) * perPage)
+            // limit the amount of items retrieved to 2 items
+            .limit(perPage)
+            // needed to show the author name on the frontend
+            // since it expects post.creator.name
+            .populate('creator');
+
+        res.status(200).json({
+            message: 'Fetched posts successfully.',
+            posts,
+            totalItems
+        });
+    } catch (err) {
+        // server-side error
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        // ASYNC CODE => throwing an error doesn't work here
+        // (it doesn't look for the next error-handling middleware)
+        next(err);
+    }
 };
 
-exports.createPost = (req, res, next) => {
+exports.createPost = async (req, res, next) => {
     // extract validation errors from route middleware
     const errors = validationResult(req);
 
@@ -89,73 +82,57 @@ exports.createPost = (req, res, next) => {
         creator: req.userId
     });
 
-    // save post to DB
-    post
-        .save()
-        .then(result => {
-            // add post to list of posts from the given user
-            return User.findById(req.userId)
-        })
-        .then(user => {
-            creator = user;
-            user.posts.push(post);
-            return user.save();
-        })
-        .then(result => {
-            res.status(201).json({
-                message: 'Post created successfully!',
-                post,
-                creator: {
-                    _id: creator._id,
-                    name: creator.name
-                }
-            });
-        })
-        .catch(err => {
-            // server-side error
-            if (!err.statusCode) {
-                err.statusCode = 500;
+    try {
+        await post.save();
+        const user = await User.findById(req.userId);
+        await user.posts.push(post);
+        await user.save();
+
+        res.status(201).json({
+            message: 'Post created successfully!',
+            post,
+            creator: {
+                _id: user._id,
+                name: user.name
             }
-            // ASYNC CODE => throwing an error doesn't work here
-            // (it doesn't look for the next error-handling middleware)
-            next(err);
         });
+    } catch (err) {
+        // server-side error
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        // ASYNC CODE => throwing an error doesn't work here
+        // (it doesn't look for the next error-handling middleware)
+        next(err);
+    }
 };
 
-exports.getPost = (req, res, next) => {
+exports.getPost = async (req, res, next) => {
     const postId = req.params.postId;
 
-    Post
-        .findById(postId)
-        // needed to show the author name on the frontend
-        // since it expects post.creator.name
-        .populate('creator')
-        .then(post => {
-            // no post was found
-            if (!post) {
-                const error = new Error('Could not find post.');
-                error.statusCode = 404;
-                // even if I'm inside then (ASYNC CODE), throwing the error
-                // means that the catch() block will execute, so next(error)
-                // is not needed here
-                throw error;
-            }
+    try {
+        // populate 'creator' since frontend expects post.creator.name
+        const post = await Post.findById(postId).populate('creator');
 
-            // post was found
-            res.status(200).json({ message: 'Post fetched.', post });
-        })
-        .catch(err => {
-            // server-side error
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            // ASYNC CODE => throwing an error doesn't work here
-            // (it doesn't look for the next error-handling middleware)
-            next(err);
-        });
+        if (!post) {
+            const error = new Error('Could not find post.');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        res.status(200).json({ message: 'Post fetched.', post });
+    } catch (err) {
+        // server-side error
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        // ASYNC CODE => throwing an error doesn't work here
+        // (it doesn't look for the next error-handling middleware)
+        next(err);
+    }
 };
 
-exports.updatePost = (req, res, next) => {
+exports.updatePost = async (req, res, next) => {
     const postId = req.params.postId;
 
     // extract validation errors from route middleware
@@ -186,160 +163,148 @@ exports.updatePost = (req, res, next) => {
         throw Error;
     }
 
-    Post
-        .findById(postId)
-        .then(post => {
-            // no post was found
-            if (!post) {
-                const error = new Error('Could not find post.');
-                error.statusCode = 404;
-                // even if I'm inside then (ASYNC CODE), throwing the error
-                // means that the catch() block will execute, so next(error)
-                // is not needed here
-                throw error;
-            }
+    try {
+        const post = await Post.findById(postId)
 
-            // check if post belongs to the currently logged in user
-            if (post.creator.toString() !== req.userId) {
-                const error = new Error('Not authorized.');
-                error.statusCode = 403;
-                throw error;
-            }
+        // no post was found
+        if (!post) {
+            const error = new Error('Could not find post.');
+            error.statusCode = 404;
+            // even if I'm inside then (ASYNC CODE), throwing the error
+            // means that the catch() block will execute, so next(error)
+            // is not needed here
+            throw error;
+        }
 
-            // user uploaded a new image => clear old image from filesystem
-            if (imageUrl !== post.imageUrl) {
-                clearImage(post.imageUrl);
-            }
+        // check if post belongs to the currently logged in user
+        if (post.creator.toString() !== req.userId) {
+            const error = new Error('Not authorized.');
+            error.statusCode = 403;
+            throw error;
+        }
 
-            // post was found => update it
-            post.title = title;
-            post.imageUrl = imageUrl;
-            post.content = content;
-            return post.save();
+        // user uploaded a new image => clear old image from filesystem
+        if (imageUrl !== post.imageUrl) {
+            clearImage(post.imageUrl);
+        }
+
+        // post was found => update it
+        post.title = title;
+        post.imageUrl = imageUrl;
+        post.content = content;
+
+        const result = await post.save();
+
+        res.status(200).json({
+            message: 'Post updated.',
+            post: result
         })
-        .then(result => {
-            res.status(200).json({
-                message: 'Post updated.',
-                post: result
-            })
-        })
-        .catch(err => {
-            // server-side error
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            // ASYNC CODE => throwing an error doesn't work here
-            // (it doesn't look for the next error-handling middleware)
-            next(err);
-        });
+    } catch (err) {
+        // server-side error
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        // ASYNC CODE => throwing an error doesn't work here
+        // (it doesn't look for the next error-handling middleware)
+        next(err);
+    }
 };
 
-exports.deletePost = (req, res, next) => {
+exports.deletePost = async (req, res, next) => {
     const postId = req.params.postId;
 
-    Post
-        .findById(postId)
-        .then(post => {
-            // check logged in user
+    try {
+        const post = await Post.findById(postId);
 
-            // no post was found
-            if (!post) {
-                const error = new Error('Could not find post.');
-                error.statusCode = 404;
-                // even if I'm inside then (ASYNC CODE), throwing the error
-                // means that the catch() block will execute, so next(error)
-                // is not needed here
-                throw error;
-            }
+        // no post was found
+        if (!post) {
+            const error = new Error('Could not find post.');
+            error.statusCode = 404;
+            // even if I'm inside then (ASYNC CODE), throwing the error
+            // means that the catch() block will execute, so next(error)
+            // is not needed here
+            throw error;
+        }
 
-            // check if post belongs to the currently logged in user
-            if (post.creator.toString() !== req.userId) {
-                const error = new Error('Not authorized.');
-                error.statusCode = 403;
-                throw error;
-            }
+        // check if post belongs to the currently logged in user
+        if (post.creator.toString() !== req.userId) {
+            const error = new Error('Not authorized.');
+            error.statusCode = 403;
+            throw error;
+        }
 
-            clearImage(post.imageUrl);
+        clearImage(post.imageUrl);
 
-            return Post.findByIdAndRemove(postId);
-        })
-        .then(result => {
-            return User.findById(req.userId);
-        })
-        .then(user => {
-            // remove deleted post from user.posts
-            user.posts.pull(postId);
-            return user.save();
-        })
-        .then(result => {
-            res.status(200).json({ message: 'Deleted post.' });
-        })
-        .catch(err => {
-            // server-side error
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            // ASYNC CODE => throwing an error doesn't work here
-            // (it doesn't look for the next error-handling middleware)
-            next(err);
-        });
+        await Post.findByIdAndRemove(postId);
+        const user = await User.findById(req.userId);
+        // remove deleted post from user.posts
+        await user.posts.pull(postId);
+        await user.save();
+
+        res.status(200).json({ message: 'Deleted post.' });
+    } catch (err) {
+        // server-side error
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        // ASYNC CODE => throwing an error doesn't work here
+        // (it doesn't look for the next error-handling middleware)
+        next(err);
+    }
 };
 
-exports.getStatus = (req, res, next) => {
-    User
-        .findById(req.userId)
-        .then(user => {
-            if (!user) {
-                const error = new Error('User not found.');
-                error.statusCode = 404;
-                throw error;
-            }
-            
-            res.status(200).json({
-                message: 'Retrieved status successfully!',
-                status: user.status
-            });
-        })
-        .catch(err => {
-            // server-side error
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            // ASYNC CODE => throwing an error doesn't work here
-            // (it doesn't look for the next error-handling middleware)
-            next(err);
+exports.getStatus = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.userId);
+
+        if (!user) {
+            const error = new Error('User not found.');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        res.status(200).json({
+            message: 'Retrieved status successfully!',
+            status: user.status
         });
+    } catch (err) {
+        // server-side error
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        // ASYNC CODE => throwing an error doesn't work here
+        // (it doesn't look for the next error-handling middleware)
+        next(err);
+    }
 };
 
-exports.updateStatus = (req, res, next) => {
+exports.updateStatus = async (req, res, next) => {
     const newStatus = req.body.status;
 
-    User
-        .findById(req.userId)
-        .then(user => {
-            if (!user) {
-                const error = new Error('User not found.');
-                error.statusCode = 404;
-                throw error;
-            }
+    try {
+        const user = await User.findById(req.userId);
 
-            user.status = newStatus;
-            return user.save();
-        })
-        .then(() => {
-            res.status(200).json({
-                message: 'Updated status successfully!'
-            });
-        })
-        .catch(err => {
-            // server-side error
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            // ASYNC CODE => throwing an error doesn't work here
-            // (it doesn't look for the next error-handling middleware)
-            next(err);
+        if (!user) {
+            const error = new Error('User not found.');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        user.status = newStatus;
+        await user.save();
+
+        res.status(200).json({
+            message: 'Updated status successfully!'
         });
+    } catch (err) {
+        // server-side error
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        // ASYNC CODE => throwing an error doesn't work here
+        // (it doesn't look for the next error-handling middleware)
+        next(err);
+    }
 };
 
 const clearImage = filePath => {
